046874b6e675264a87b309194979b67551397c73
amithkashyap <amithkashyaprv@gmail.com>
2025-05-16 23:01:02 +0530

    added testimonials page

---
+++ src/app/testimonials/page.tsx
@@ -0,0 +1,17 @@
+import Navigation from "@/components/Navigation";
+import Testimonial from "@/components/Testimonial";
+
+const TestimonialsPage = () => {
+  return (
+    <>
+      <div className="h-screen bg-gray-600 bg-[url(/deepthi-cover-2.jpg)] bg-blend-overlay">
+        <Navigation />
+        <div className="flex flex-col items-center justify-center">
+          <Testimonial />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TestimonialsPage;
