2b0c3d96b58a0361469c998100ac9983ed6ad4c8
amithkashyap <amithkashyaprv@gmail.com>
2025-05-24 23:53:37 +0530

    modified about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
         <div className="flex items-center w-[1200px] mx-auto gap-16 mt-32">
           <div className="">
             <Image
-              src="/amith.png"
+              src="/deepthi-cover.jpeg"
               alt="Amith"
               width={700}
               height={700}
@@ -17,21 +17,21 @@ const AboutPage = () => {
             />
           </div>
           <div className="text-white w-[70%]">
-            <h1 className="text-5xl">Hi there!</h1>
-            <p className="text-8xl"> I'm Deepthi</p>
-            <p className="leading-[1.9] mt-6">
+            <h1 className="text-5xl animate-fade-in-left">Hi there!</h1>
+            <p className="text-8xl animate-fade-in-right"> I'm Deepthi</p>
+            <p className="leading-[1.9] mt-6 animate-fade-in-bottom">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
               numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Labore, laborum! Lorem ipsum, dolor sit amet consectetur
               adipisicing elit. Nam animi dolores pariatur mollitia voluptas id
               fuga ullam quos! Totam, praesentium!
             </p>
-            <p className="leading-[1.9] mt-6">
+            <p className="leading-[1.9] mt-6 animate-fade-in-bottom">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
               numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Labore, laborum!
             </p>
-            <button className="border-none px-8 py-4 bg-amber-500 text-2xl rounded-3xl mt-8 hover:bg-amber-600 hover:text-white cursor-pointer">
+            <button className="border-none px-8 py-4 bg-amber-500 text-2xl rounded-xl mt-8 hover:bg-amber-600 hover:text-white cursor-pointer">
               View Portfolio
             </button>
           </div>
