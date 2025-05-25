4c3535dbd0902c9d2b3fbaf2d3b7b8ad4b975c09
amithkashyap <amithkashyaprv@gmail.com>
2025-05-25 19:44:03 +0530

    refactored navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,39 +3,37 @@ import Link from "next/link";
 
 const Navigation = () => {
   return (
-    <div className="">
-      <div className="flex w-[1200px] justify-between m-auto bg-gray-950 opacity-50 py-6 px-12 rounded-3xl text-amber-100 hover:opacity-75">
-        <div className="flex gap-2">
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          <Link className="text-xl hover:text-yellow-100 " href="/">
-            Deepthi
-          </Link>
-        </div>
-        <div>
-          <ul className="flex gap-12 text-xl">
-            <li className="hover:text-white">
-              <Link href="/about">About</Link>
-            </li>
-            <li className="hover:text-white">
-              <Link href="/work-experience">Work Experience</Link>
-            </li>
-            <li className="hover:text-white">
-              <Link href="/projects">Projects</Link>
-            </li>
-            <li className="hover:text-white">
-              <Link href="/testimonials">Testimonials</Link>
-            </li>
-            <li className="hover:text-white">
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
+    <div className="flex w-[80%] justify-between mx-auto bg-gray-950 py-6 px-12 rounded-3xl text-white uppercase tracking-wider">
+      <div className="flex gap-2">
+        <Image
+          aria-hidden
+          src="/globe.svg"
+          alt="Globe icon"
+          width={16}
+          height={16}
+        />
+        <Link className="text-xl hover:text-yellow-100 " href="/">
+          Deepthi
+        </Link>
+      </div>
+      <div>
+        <ul className="flex gap-12 text-[16px]">
+          <li className="hover:text-white">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="hover:text-white">
+            <Link href="/work-experience">Work Experience</Link>
+          </li>
+          <li className="hover:text-white">
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li className="hover:text-white">
+            <Link href="/testimonials">Testimonials</Link>
+          </li>
+          <li className="hover:text-white">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
