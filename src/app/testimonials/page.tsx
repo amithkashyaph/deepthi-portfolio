d554b8801cc3eb74544a1b0f9213b0ba0a31c4a1
amithkashyap <amithkashyaprv@gmail.com>
2025-05-29 00:35:39 +0530

    refactored testimonial component

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -1,13 +1,14 @@
 import Navigation from "@/components/Navigation";
 import Testimonial from "@/components/Testimonial";
+import TestimonialSlider from "@/components/TestimonialSlider";
 
 const TestimonialsPage = () => {
   return (
     <>
       <div className="h-screen bg-gray-600 bg-[url(/deepthi-cover-2.jpg)] bg-blend-overlay">
         <Navigation />
-        <div className="flex flex-col items-center justify-center">
-          <Testimonial />
+        <div className="min-h-screen  flex items-center justify-center">
+          <TestimonialSlider />
         </div>
       </div>
     </>
