d3787aa099b73dd6d5721b77a3692197e88bd28b
amithkashyap <amithkashyaprv@gmail.com>
2025-06-06 23:55:08 +0530

    added image row component

---
+++ src/components/ImageRow.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+interface ImageRowProps {
+  images: string[];
+  title: string;
+}
+
+const ImageRow: React.FC<ImageRowProps> = ({ images, title }) => {
+  return (
+    <div className="relative">
+      {/* <div className="absolute top-24 left-0 w-full h-[400px]  z-20 bg-gradient-to-b from-[rgb(14,5,5)] to-[rgb(41, 13, 18)] " /> */}
+      <div
+        className={`absolute inset-0 top-20 z-0 ${
+          images && images.length > 2
+            ? "bg-gradient-to-b from-[rgb(14,5,5)] to-[rgb(29,8,12)]"
+            : "bg-black"
+        }`}
+      />
+      <div className="-mt-20 relative z-40 px-6">
+        <div className="flex space-x-6 overflow-x-auto scrollbar-hide pb-4 px-18 justify-start ">
+          {images.map((src, idx) => (
+            <div
+              key={idx}
+              className="relative  md:max-w-[250px]  bg-neutral-800 rounded-xl overflow-hidden flex-shrink-0 hover:scale-105 transition-transform duration-300 shadow-md max-h-[300px]"
+            >
+              {/* Image */}
+              <img
+                src={src}
+                alt={`Project ${idx + 1}`}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageRow;
