01bd56769c3dc826c9203fdfd36120d445f7d8cb
amithkashyap <amithkashyaprv@gmail.com>
2025-06-08 15:02:26 +0530

    refactored ImageRow component

--- src/components/ImageRow.tsx
+++ src/components/ImageRow.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 interface ImageRowProps {
@@ -5,9 +6,9 @@ interface ImageRowProps {
   title: string;
 }
 
-const ImageRow: React.FC<ImageRowProps> = ({ images, title }) => {
+const ImageRow: React.FC<ImageRowProps> = ({ images }) => {
   return (
-    <div className="relative">
+    <div className="relative h-[400px]">
       {/* <div className="absolute top-24 left-0 w-full h-[400px]  z-20 bg-gradient-to-b from-[rgb(14,5,5)] to-[rgb(41, 13, 18)] " /> */}
       <div
         className={`absolute inset-0 top-20 z-0 ${
@@ -17,17 +18,20 @@ const ImageRow: React.FC<ImageRowProps> = ({ images, title }) => {
         }`}
       />
       <div className="-mt-20 relative z-40 px-6">
-        <div className="flex space-x-6 overflow-x-auto scrollbar-hide pb-4 px-18 justify-start ">
+        <div className="flex space-x-6 overflow-x-auto scrollbar-hide pb-4 px-18 justify-start">
           {images.map((src, idx) => (
             <div
               key={idx}
               className="relative  md:max-w-[250px]  bg-neutral-800 rounded-xl overflow-hidden flex-shrink-0 hover:scale-105 transition-transform duration-300 shadow-md max-h-[300px]"
             >
               {/* Image */}
-              <img
+              <Image
                 src={src}
                 alt={`Project ${idx + 1}`}
                 className="w-full h-full object-cover"
+                sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+                height={400}
+                width={200}
               />
             </div>
           ))}
