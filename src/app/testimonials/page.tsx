2d78cdab225ab8063e3bdfbf945eb376b80e218a
amithkashyap <amithkashyaprv@gmail.com>
2025-06-08 15:00:48 +0530

    refactored bg-url

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -1,11 +1,10 @@
 import Navigation from "@/components/Navigation";
-import Testimonial from "@/components/Testimonial";
 import TestimonialSlider from "@/components/TestimonialSlider";
 
 const TestimonialsPage = () => {
   return (
     <>
-      <div className="h-screen bg-gray-600 bg-[url(/deepthi-cover-2.jpg)] bg-blend-overlay">
+      <div className="h-screen bg-gray-600 bg-[url(/deepthi-cover-2.JPG)] bg-blend-overlay">
         <Navigation />
         <div className="min-h-screen  flex items-center justify-center">
           <TestimonialSlider />
