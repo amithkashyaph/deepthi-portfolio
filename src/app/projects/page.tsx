8e1bdeaaf1e9c1b92799d3c157c24b66a467e048
amithkashyap <amithkashyaprv@gmail.com>
2025-06-08 14:59:57 +0530

    refactored bg-url

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import ProjectCard from "@/components/ProjectCard";
 
 const Projects = () => {
   return (
-    <div className="h-full bg-gray-600 bg-[url(/deepthi-cover-2.jpg)] bg-blend-overlay">
+    <div className="h-full bg-gray-600 bg-[url(/deepthi-cover-2.JPG)] bg-blend-overlay">
       <Navigation />
       <ProjectCard />
     </div>
