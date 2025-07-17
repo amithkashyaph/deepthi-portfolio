a2eb5852a2c0d18ea6754bf3cd4592f7c4af70d2
amithkashyap <amithkashyaprv@gmail.com>
2025-07-17 22:41:36 +0530

    added initial under construction page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+// app/your-page-name/page.tsx (or pages/your-page-name.tsx for Pages Router)
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Under Construction",
+};
+
+export default function UnderConstructionPage() {}
